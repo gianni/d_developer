91af47962083b3d436e68553a99090f991ce1b7a
gianni <fiorentinig@gmail.com>
2017-05-03 17:34:28 +0200

    set/unset cookies

--- popup.js
+++ popup.js
@@ -30,19 +30,38 @@ chrome.storage.sync.get({
     function removecookie() {
         chrome.cookies.remove({"url": "http://d2."+items.domain, "name": "developer"}, function(deleted_cookie) {});
         chrome.cookies.remove({"url": "http://s2."+items.domain, "name": "developer"}, function(deleted_cookie) {});
+        document.getElementById('d2').style="background-color:#FFFFFF";
+        document.getElementById('s2').style="background-color:#FFFFFF";
     }
 
-    function copy(str) {
+    function togglecookie() {
+        var _this=this;
+
+        chrome.cookies.get({name:"developer","url":"http://"+_this.dataset.value+"."+items.domain}, function(cookie){
+            if(cookie){
+                chrome.cookies.remove({"url": "http://"+_this.dataset.value+"."+items.domain, "name": "developer"}, function(deleted_cookie) {
+                    document.getElementById(_this.dataset.value).style="background-color:#FFFFFF";
+                });
+            } else {
+                chrome.cookies.set({"url": "http://"+_this.dataset.value+"."+items.domain, "name": "developer"}, function(set_cookie) {
+                    document.getElementById(_this.dataset.value).style="background-color:#3ace61";
+                });
+            }
+        });
+    }
+
+    function copy() {
         document.oncopy = function(event) {
-            event.clipboardData.setData('text/plain', str);
+            event.clipboardData.setData('text/plain', items.pwd_1);
             event.preventDefault();
         };
         document.execCommand("Copy", false, null);
     }
 
     document.getElementById('settings').addEventListener('click', goSettings);
-    document.getElementById('removed2s2').addEventListener('click', removecookie);
-    document.getElementById('pwd_1').addEventListener('click', copy(items.pwd_1));
+    document.getElementById('pwd_1').addEventListener('click', copy);
+    document.getElementById('s2').addEventListener('click', togglecookie);
+    document.getElementById('d2').addEventListener('click', togglecookie);
 
 });
 
